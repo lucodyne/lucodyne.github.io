29a43f50fcb992fd4864712e7688c3f94f1155cc
Sean Lim <lim_s@outlook.com>
2020-02-06 16:06:56 -0600

    import components and react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import "./App.css";
+import About from "./pages/About";
+import Header from "./components/Header";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
-function App() {
-  return <></>;
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <Router>
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/projects">
+              <Projects />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+          </Switch>
+        </Router>
+      </>
+    );
+  }
 }
 
 export default App;
