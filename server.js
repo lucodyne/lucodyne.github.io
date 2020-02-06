38b9e21fe8521a297e873dcacfbb48903e0d6491
Sean Lim <lim_s@outlook.com>
2020-02-06 16:05:49 -0600

    no routes

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const PORT = process.env.PORT || 6072;
 const app = express();
-const routes = require("./routes");
+
 app.use(express.json());
 app.use(express.static("client/build"));
 app.use(express.urlencoded({ extended: true }));
-app.use(routes);
+
 app.listen(PORT, function() {
   console.log("App now listening: " + PORT);
 });
