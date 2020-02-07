e66a1fe5f98340c796910e0029c3a7598c74a078
Sean Lim <lim_s@outlook.com>
2020-02-06 19:40:20 -0600

    sidenav and homepage included

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,22 @@ import About from "./pages/About";
 import Header from "./components/Header";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
+import Sidenav from "./components/Sidenav";
+import Home from "./pages/Home";
 
 class App extends Component {
   render() {
     return (
       <>
         <Header />
+        <Sidenav />
         <Router>
           <Switch>
             <Route path="/about">
               <About />
+            </Route>
+            <Route path="/">
+              <Home />
             </Route>
             <Route path="/projects">
               <Projects />
