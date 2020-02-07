d6402f4f3842d381076fda2e220507757f2c7a2a
Sean Lim <lim_s@outlook.com>
2020-02-06 19:40:38 -0600

    pages

---
+++ client/src/pages/About.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class About extends Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="col m0 l2"></div>
+        <div className="col s12 m12 l8">
+          <div className="card grey darken-1 grey-text text-lighten-2">
+            <h3>About Me</h3>
+            <p>
+              words words words words words words words words words words words
+              words words words words words words words words words words words
+              words words
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default About;
