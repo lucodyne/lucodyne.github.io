00d5907f42ca9266ab19543f3759a6c328bc7f2c
Sean Lim <lim_s@outlook.com>
2020-02-10 15:35:44 -0600

    route troubleshooting

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.use(express.json());
 app.use(express.static("client/build"));
 app.use(express.urlencoded({ extended: true }));
 
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 app.listen(PORT, function() {
   console.log("App now listening: " + PORT);
 });
