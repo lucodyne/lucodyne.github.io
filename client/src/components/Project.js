05b54f47be0dbdba4d394a3a54a592ac48d8a951
Sean Lim <lim_s@outlook.com>
2020-02-10 15:56:19 -0600

    bugfixing maybe

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -3,12 +3,12 @@ import React, { Component } from "react";
 class Project extends Component {
   render() {
     return (
-      <div className="card hoverable left col s6 m4 l3">
+      <div className="card hoverable col s6 m4 l3">
         <div className="card-image">
           <img src={this.props.image}></img>
-          <span className="card-title">{this.props.name}</span>
+          <span className="card-title cyan-text bold">{this.props.name}</span>
         </div>
-        <div className="card-content">
+        <div className="card-content flow-text">
           <p>{this.props.description}</p>
         </div>
         <div className="card-action">
--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const PORT = process.env.PORT || 6072;
 const app = express();
 
 app.use(express.json());
-app.use(express.static("client/build"));
+app.use("*", express.static("client/build"));
 app.use(express.urlencoded({ extended: true }));
 
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.listen(PORT, function() {
   console.log("App now listening: " + PORT);
