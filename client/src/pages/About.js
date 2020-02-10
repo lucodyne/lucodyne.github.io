7601459e3997f13492f81cc8658dbd05e8172ca2
Sean Lim <lim_s@outlook.com>
2020-02-10 10:33:27 -0600

    breaks for spacing

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -7,12 +7,14 @@ class About extends Component {
         <div className="col m0 l2"></div>
         <div className="col s12 m12 l8">
           <div className="card grey darken-1 grey-text text-lighten-2">
+            <br />
             <h3>About Me</h3>
             <p>
               words words words words words words words words words words words
               words words words words words words words words words words words
               words words
             </p>
+            <br />
           </div>
         </div>
       </div>
