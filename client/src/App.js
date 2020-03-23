f924a935a373e9554987ecb486110bce3b2f7cc1
Sean Lim <lim_s@outlook.com>
2020-03-23 08:35:08 -0500

    removed header, sidenav, and router. single page

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,25 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import About from "./pages/About";
-import Header from "./components/Header";
+// import Header from "./components/Header";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
-import Sidenav from "./components/Sidenav";
-import Home from "./pages/Home";
+// import Sidenav from "./components/Sidenav";
 
 class App extends Component {
   render() {
     return (
       <>
-        <Header />
-        <Sidenav />
-        <Router>
-          <Switch>
-            {/* <Route path="/about">
-              <About />
-            </Route> */}
-            <Route path="/projects">
-              <Projects />
-            </Route>
-            <Route path="/contact">
-              <Contact />
-            </Route>
-            <Route path="/">
-              <About />
-            </Route>
-          </Switch>
-        </Router>
+        {/* <Header />
+
+        <Sidenav /> */}
+
+        <About />
+
+        <Projects />
+
+        <Contact />
       </>
     );
   }
