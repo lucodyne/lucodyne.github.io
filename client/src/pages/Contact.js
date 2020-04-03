e34059d31a976293ba54c982969af3a86b6a97ec
Sean Lim <lim_s@outlook.com>
2020-04-03 17:39:48 -0500

    some styling, and removed portfolio link lol

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -5,29 +5,34 @@ class Contact extends Component {
     return (
       <div className="row">
         <div className="col s1 m2"></div>
-        <div className="card col s10 m8 center grey darken-1 grey-text text-lighten-2">
+        <div className="card col s10 m8 center grey darken-1">
           {/* <h3>Contact</h3> */}
 
           <ul>
             <li>
               email:{" "}
-              <a href="mailto:hi.im.sean.lim@gmail.com">
+              <a
+                className="cyan-text text-lighten-2"
+                href="mailto:hi.im.sean.lim@gmail.com"
+              >
                 hi.im.sean.lim@gmail.com
               </a>
             </li>
             <li>
               github:{" "}
-              <a href="https://github.com/lucodyne">github.com/lucodyne</a>
-            </li>
-            <li>
-              portfolio:{" "}
-              <a href="https://nameless-scrubland-68002.herokuapp.com/">
-                nameless-scrubland-68002.herokuapp.com/
+              <a
+                className="cyan-text text-lighten-2"
+                href="https://github.com/lucodyne"
+              >
+                github.com/lucodyne
               </a>
             </li>
             <li>
               linkedin:{" "}
-              <a href="https://www.linkedin.com/in/sean-lim-8126ab1a0/">
+              <a
+                className="cyan-text text-lighten-2"
+                href="https://www.linkedin.com/in/sean-lim-8126ab1a0/"
+              >
                 www.linkedin.com/in/sean-lim-8126ab1a0/
               </a>
             </li>
