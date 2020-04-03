f5e8df0707922255ae9988609cd1c38eb476da65
Sean Lim <lim_s@outlook.com>
2020-04-03 17:40:50 -0500

    fixed the spacing, maybe

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -4,28 +4,37 @@ class Project extends Component {
   render() {
     return (
       <>
-        <div className="card col s6 m3 l3">
-          <div className="card-image waves-effect waves-block waves-light">
-            <img className="activator" src={this.props.image}></img>
+        <div className="col s12 m6 l6">
+          <div className="col s0 m1 l1"></div>
+          <div className="card grey darken-1 col s12 m10 l9 ">
+            <div className="card-image waves-effect waves-block waves-light">
+              <img className="activator" src={this.props.image}></img>
+            </div>
+            <div className="card-content flow-text">
+              <span className="center card-title activator">
+                {this.props.name}
+                <i class="material-icons right">more_vert</i>
+              </span>
+            </div>
+            <div className="card-reveal grey darken-1 cyan-text text-lighten-2">
+              <span className="card-title center grey-text text-darken-4 flow-text">
+                {this.props.name}
+                <i class="material-icons right">close</i>
+              </span>
+              <p>{this.props.description}</p>
+              <br></br>
+              <p className="grey-text text-darken-4">
+                tools used: {this.props.tools}
+              </p>
+              {/* <p>role: {this.props.position}</p> */}
+            </div>
+            <div className="card-action">
+              <a className="cyan-text text-lighten-2" href={this.props.url}>
+                link
+              </a>
+            </div>
           </div>
-          <div className="card-content flow-text">
-            <span className="center card-title activator">
-              {this.props.name}
-              <i class="material-icons right">more_vert</i>
-            </span>
-          </div>
-          <div className="card-reveal">
-            <span className="card-title center flow-text">
-              {this.props.name}
-              <i class="material-icons right">close</i>
-            </span>
-            <p>{this.props.description}</p>
-            <p>tools used: {this.props.tools}</p>
-            <p>role: {this.props.position}</p>
-          </div>
-          <div className="card-action">
-            <a href={this.props.url}>go</a>
-          </div>
+          {/* <div className="col s0 m1"></div> */}
         </div>
       </>
     );
